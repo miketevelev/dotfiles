2397693e571fb5986ac0b7a4a2a1de537af092d7
Mike <miketevelev@gmail.com>
2025-05-09 17:33:48 +0300

    first commit

---
+++ .emacs.custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(custom-enabled-themes '(gruber-darker))
+ '(custom-line-number-type 'relative)
+
+ '(display-line-numbers-type (quote relative))
+ '(org-agenda-dim-blocked-tasks nil)
+ '(org-agenda-exporter-settings (quote ((org-agenda-tag-filter-preset (list "+personal")))))
+ '(org-cliplink-transport-implementation (quote url-el))
+ '(org-enforce-todo-dependencies nil)
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(org-refile-use-outline-path (quote file))
+ '(package-selected-packages
+   (quote
+    (rainbow-mode proof-general elpy hindent ag qml-mode racket-mode php-mode go-mode kotlin-mode nginx-mode toml-mode love-minor-mode dockerfile-mode nix-mode purescript-mode markdown-mode jinja2-mode nim-mode csharp-mode rust-mode cmake-mode clojure-mode graphviz-dot-mode lua-mode tuareg glsl-mode yaml-mode d-mode scala-mode move-text nasm-mode editorconfig tide company powershell js2-mode yasnippet helm-ls-git helm-git-grep helm-cmd-t helm multiple-cursors magit haskell-mode paredit ido-completing-read+ smex gruber-darker-theme org-cliplink dash-functional dash)))
+ '(warning-minimum-level :error)
+ 
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+	 default))
+ '(indent-tabs-mode t)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
